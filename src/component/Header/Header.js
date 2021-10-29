8abdbdd58da25fd54b4aeb41c6d2aeb77d2d863c
Your Name <you@example.com>
2021-10-30 01:24:24 +0330

    change header icons

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,16 +1,13 @@
-import moonImg from "../../assets/images/icon-moon.svg";
-import sunImg from "../../assets/images/icon-sun.svg";
+import { BiMoon, BiSun } from "react-icons/bi";
 import styles from "./Header.module.css";
 
 const Header = ({ darkMode, onModeChange }) => {
   return (
     <div className={styles.header}>
       <h1> TODO </h1>
-      <img
-        src={darkMode ? moonImg : sunImg}
-        alt="read mode"
-        onClick={onModeChange}
-      />
+      <button onClick={onModeChange} className={styles.mode_icon}>
+        {darkMode ? <BiMoon /> : <BiSun />}
+      </button>
     </div>
   );
 };
